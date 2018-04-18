5a48de237196cb5ddac155219ade7e9c8a285f2e
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-18 18:46:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,11 @@ def key_for_min_value(name_hash)
   min_key = "hi"
 name_hash.each do |key, value|
   if min_value == "hello"
-    "#{key}" = min_key
-    "#{value}" = min_value
+    min_key = key 
+    min_value = value
   elsif "#{value}" < min_value
-    "#{value}" = min_value
-    "#{key}" = min_key
+    min_value = value
+    min_key = key
   end
 end
 end
